cb4bdf98515e4aac86e4dbe1836223816a8a076c
Ansh Jaiswal <mailansh@proton.me>
2024-04-11 15:14:04 +0530

    2

--- register.php
+++ register.php
@@ -1,17 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    
-    $data = "Username: $username\nEmail: $email\nPassword: $password\n\n";
-    
-    // Append the data to a text file
-    $file = fopen("registrations.txt", "a");
-    fwrite($file, $data);
-    fclose($file);
-    
-    // Redirect to the second page
+    // Get the form data
+    $username = $_POST["username"] ?? '';
+    $email = $_POST["email"] ?? '';
+    $password = $_POST["password"] ?? '';
+
+    // Prepare the log message
+    $logMessage = "[" . date("Y-m-d H:i:s") . "] - Username: $username, Email: $email, Password: $password" . PHP_EOL;
+
+    // Write the log message to the log file
+    file_put_contents("registrations.log", $logMessage, FILE_APPEND);
+
+    // Redirect to page 2
     header("Location: page2.html");
     exit;
 }
